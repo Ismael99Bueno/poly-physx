2ddc29f2409ce2e853ba905ea13bc93d16758f9f
Ismael Bueno <ismaelwno@gmail.com>
2023-04-29 01:14:52 +0200

    Added pch files

---
+++ include/ppx/pch.hpp
@@ -0,0 +1,19 @@
+#ifndef PPX_PCH
+#define PPX_PCH
+
+#include <vector>
+#include <utility>
+#include <limits>
+#include <cmath>
+#include <algorithm>
+#include <unordered_set>
+#include <glm/geometric.hpp>
+#include <glm/gtx/norm.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <memory>
+#include <type_traits>
+#include <array>
+#include <cstdint>
+#include <variant>
+
+#endif
---
+++ src/pch.cpp
@@ -0,0 +1,1 @@
+#include "ppx/pch.hpp"
