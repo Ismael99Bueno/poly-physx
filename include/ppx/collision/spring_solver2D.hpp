d044780d0699463493c1d0f6e0d73d0cf06d53d8
Ismael Bueno <ismaelwno@gmail.com>
2023-11-06 00:47:14 +0100

    Collision solver parameters are now bound to be between 0 and 1

--- include/ppx/collision/spring_solver2D.hpp
+++ include/ppx/collision/spring_solver2D.hpp
@@ -8,14 +8,29 @@ namespace ppx
 class spring_solver2D : public collision_solver2D
 {
   public:
-    static inline float stiffness = 5000.f;
-    static inline float dampening = 10.f;
-
     void solve(const std::vector<collision2D> &collisions) const override;
     void solve_and_apply_collision_forces(const collision2D &colis) const;
 
+    static float rigidity_coeff();
+    static float restitution_coeff();
+    static float friction_coeff();
+
+    static void rigidity_coeff(float rigidity);
+    static void restitution_coeff(float restitution);
+    static void friction_coeff(float friction);
+
+    static float rigidity();
+    static float restitution();
+    static float friction();
+
   private:
-    std::array<float, 6> forces_upon_collision(const collision2D &colis) const;
+    static inline float s_rigidity_coeff = 0.985f;
+    static inline float s_restitution_coeff = 0.4f;
+    static inline float s_friction_coeff = 0.6f;
+
+    static float s_rigidity;
+    static float s_restitution;
+    static float s_friction;
 };
 } // namespace ppx
 
--- src/collision/spring_solver2D.cpp
+++ src/collision/spring_solver2D.cpp
@@ -13,6 +13,64 @@
 
 namespace ppx
 {
+static float map_zero_one_zero_inf(const float x, const float slope)
+{
+    return slope * logf((1 + x) / (1 - x));
+}
+static float map_one_zero_zero_inf(const float x, const float slope)
+{
+    return slope * logf((2 - x) / x);
+}
+
+float spring_solver2D::s_rigidity = map_zero_one_zero_inf(spring_solver2D::s_rigidity_coeff, 400.f);
+float spring_solver2D::s_restitution = map_zero_one_zero_inf(spring_solver2D::s_restitution_coeff, 2.f);
+float spring_solver2D::s_friction = map_zero_one_zero_inf(spring_solver2D::s_friction_coeff, 2.f);
+
+float spring_solver2D::rigidity_coeff()
+{
+    return s_rigidity_coeff;
+}
+float spring_solver2D::restitution_coeff()
+{
+    return s_restitution_coeff;
+}
+float spring_solver2D::friction_coeff()
+{
+    return s_friction_coeff;
+}
+
+void spring_solver2D::rigidity_coeff(const float rigidity)
+{
+    KIT_ASSERT_ERROR(rigidity >= 0.f && rigidity < 1.f, "Rigidity must lie between [0, 1)")
+    s_rigidity_coeff = rigidity;
+    s_rigidity = map_zero_one_zero_inf(rigidity, 200.f);
+}
+void spring_solver2D::restitution_coeff(const float restitution)
+{
+    KIT_ASSERT_ERROR(restitution > 0.f && restitution <= 1.f, "Restitution must lie between (0, 1]")
+    s_restitution_coeff = restitution;
+    s_restitution = map_one_zero_zero_inf(restitution, 2.f);
+}
+void spring_solver2D::friction_coeff(const float friction)
+{
+    KIT_ASSERT_ERROR(friction >= 0.f && friction < 1.f, "Friction must lie between [0, 1]")
+    s_friction_coeff = friction;
+    s_friction = map_zero_one_zero_inf(friction, 2.f);
+}
+
+float spring_solver2D::rigidity()
+{
+    return s_rigidity;
+}
+float spring_solver2D::restitution()
+{
+    return s_restitution;
+}
+float spring_solver2D::friction()
+{
+    return s_friction;
+}
+
 void spring_solver2D::solve(const std::vector<collision2D> &collisions) const
 {
     KIT_PERF_FUNCTION()
@@ -32,9 +90,18 @@ void spring_solver2D::solve(const std::vector<collision2D> &collisions) const
 void spring_solver2D::solve_and_apply_collision_forces(const collision2D &colis) const
 {
     KIT_PERF_FUNCTION()
-    const glm::vec2 rel1 = colis.touch1 - colis.current->position(), rel2 = colis.touch2 - colis.incoming->position();
-    const glm::vec2 vel1 = colis.current->velocity_at(rel1), vel2 = colis.incoming->velocity_at(rel2);
-    const glm::vec2 force = stiffness * (colis.touch2 - colis.touch1) + dampening * (vel2 - vel1);
+    const glm::vec2 rel1 = colis.touch1 - colis.current->position();
+    const glm::vec2 rel2 = colis.touch2 - colis.incoming->position();
+
+    const glm::vec2 relvel = (colis.incoming->velocity_at(rel2) - colis.current->velocity_at(rel1));
+    const glm::vec2 reltouch = colis.touch2 - colis.touch1;
+
+    const glm::vec2 normal_dir = glm::normalize(colis.normal);
+
+    const glm::vec2 orth_vel = glm::dot(normal_dir, relvel) * normal_dir;
+    const glm::vec2 tangent_vel = relvel - orth_vel;
+
+    const glm::vec2 force = reltouch * s_rigidity + orth_vel * s_restitution + tangent_vel * s_friction;
 
     const float torque1 = kit::cross2D(rel1, force), torque2 = kit::cross2D(force, rel2);
     colis.current->apply_simulation_force(force);
@@ -42,16 +109,5 @@ void spring_solver2D::solve_and_apply_collision_forces(const collision2D &colis)
 
     colis.incoming->apply_simulation_force(-force);
     colis.incoming->apply_simulation_torque(torque2);
-}
-
-std::array<float, 6> spring_solver2D::forces_upon_collision(const collision2D &colis) const
-{
-    KIT_PERF_FUNCTION()
-    const glm::vec2 rel1 = colis.touch1 - colis.current->position(), rel2 = colis.touch2 - colis.incoming->position();
-    const glm::vec2 vel1 = colis.current->velocity_at(rel1), vel2 = colis.incoming->velocity_at(rel2);
-    const glm::vec2 force = stiffness * (colis.touch2 - colis.touch1) + dampening * (vel2 - vel1);
-
-    const float torque1 = kit::cross2D(rel1, force), torque2 = kit::cross2D(force, rel2);
-    return {force.x, force.y, torque1, -force.x, -force.y, torque2};
 }
 } // namespace ppx
