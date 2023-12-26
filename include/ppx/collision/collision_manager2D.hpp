e147a5daf33790aee8c2e08d4ad9dcfa1c078a15
Ismael Bueno <ismaelwno@gmail.com>
2023-12-26 00:59:21 +0000

    Fixed typos in set_resolution method

--- include/ppx/collision/collision_manager2D.hpp
+++ include/ppx/collision/collision_manager2D.hpp
@@ -70,6 +70,8 @@ class collision_manager2D
                       "Detection method must inherit from collision_detection2D");
         auto coldet = kit::make_scope<T>(std::forward<ColDetArgs>(args)...);
         T *ptr = coldet.get();
+        ptr->epa_threshold = m_collision_detection->epa_threshold;
+
         m_collision_detection = std::move(coldet);
         m_collision_detection->world = &m_world;
         m_collision_detection->on_attach();
@@ -88,9 +90,10 @@ class collision_manager2D
     {
         static_assert(std::is_base_of_v<collision_resolution2D, T>,
                       "Resolution method must inherit from collision_resolution2D");
-        auto coldet = kit::make_scope<T>(std::forward<ColSolvArgs>(args)...);
-        T *ptr = coldet.get();
-        m_collision_resolution = std::move(coldet);
+        auto colres = kit::make_scope<T>(std::forward<ColSolvArgs>(args)...);
+        T *ptr = colres.get();
+
+        m_collision_resolution = std::move(colres);
         m_collision_resolution->world = &m_world;
         m_collision_resolution->on_attach();
         if constexpr (std::is_same_v<T, spring_driven_resolution2D>)
