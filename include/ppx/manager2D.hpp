7e8d539796e13375aa66d4c37c785cbc7b896025
Ismael Bueno <ismaelwno@gmail.com>
2024-01-27 01:10:58 +0100

    manager2D now accounts for kit::scope types

--- include/ppx/manager2D.hpp
+++ include/ppx/manager2D.hpp
@@ -2,15 +2,30 @@
 
 #include "ppx/internal/worldref.hpp"
 #include "kit/interface/identifiable.hpp"
+#include "kit/memory/scope.hpp"
 #include <vector>
 
 namespace ppx
 {
 class world2D;
-template <kit::Identifiable T> class manager2D
+
+template <kit::Identifiable T> struct type_wrapper
+{
+    using value = T;
+    static inline constexpr bool is_scope = false;
+};
+template <kit::Identifiable T> struct type_wrapper<kit::scope<T>>
+{
+    using value = T;
+    static inline constexpr bool is_scope = true;
+};
+
+template <typename T> class manager2D : public worldref2D
 {
   public:
-    using id_type = typename T::id_type;
+    using value_type = type_wrapper<T>::value;
+    using id_type = typename value_type::id_type;
+    static inline constexpr bool is_scope = type_wrapper<T>::is_scope;
 
     manager2D(world2D &world);
     virtual ~manager2D() = default;
@@ -33,14 +48,14 @@ template <kit::Identifiable T> class manager2D
         return m_elements.end();
     }
 
-    const T &operator[](std::size_t index) const;
-    T &operator[](std::size_t index);
+    const value_type &operator[](std::size_t index) const;
+    value_type &operator[](std::size_t index);
 
-    const T *operator[](const id_type &id) const;
-    T *operator[](const id_type &id);
+    const value_type *operator[](const id_type &id) const;
+    value_type *operator[](const id_type &id);
 
     virtual bool remove(std::size_t index) = 0;
-    bool remove(const T &element);
+    bool remove(const value_type &element);
     bool remove(const id_type &id);
 
     std::size_t size() const;
--- src/manager2D.cpp
+++ src/manager2D.cpp
@@ -1,60 +1,112 @@
 #include "ppx/internal/pch.hpp"
 #include "ppx/manager2D.hpp"
+#include "ppx/entities/body2D.hpp"
+#include "ppx/entities/collider2D.hpp"
+#include "ppx/behaviours/behaviour2D.hpp"
+#include "ppx/joints/spring2D.hpp"
+#include "ppx/constraints/constraint2D.hpp"
 
 namespace ppx
 {
-template <kit::Identifiable T> manager2D<T>::manager2D(world2D &world) : worldref2D(world)
+template <typename T> manager2D<T>::manager2D(world2D &world) : worldref2D(world)
 {
 }
 
-template <kit::Identifiable T> const T &manager2D<T>::operator[](std::size_t index) const
+template <typename T> const typename manager2D<T>::value_type &manager2D<T>::operator[](std::size_t index) const
 {
     KIT_ASSERT_ERROR(index < m_elements.size(), "Index exceeds array bounds - index: {0}, size: {1}", index,
                      m_elements.size())
-    return m_elements[index];
+
+    if constexpr (is_scope)
+        return m_elements[index];
+    else
+        return *m_elements[index];
 }
 
-template <kit::Identifiable T> T &manager2D<T>::operator[](std::size_t index)
+template <typename T> typename manager2D<T>::value_type &manager2D<T>::operator[](std::size_t index)
 {
     KIT_ASSERT_ERROR(index < m_elements.size(), "Index exceeds array bounds - index: {0}, size: {1}", index,
                      m_elements.size())
-    return m_elements[index];
+    if constexpr (is_scope)
+        return m_elements[index];
+    else
+        return *m_elements[index];
 }
 
-template <kit::Identifiable T> const T *manager2D<T>::operator[](const id_type &id) const
+template <typename T> const typename manager2D<T>::value_type *manager2D<T>::operator[](const id_type &id) const
 {
-    for (const T &element : m_elements)
-        if (element.id == id)
-            return &element;
+    if constexpr (is_scope)
+    {
+        for (const T &element : m_elements)
+            if (element->id == id)
+                return element.get();
+    }
+    else
+        for (const T &element : m_elements)
+            if (element.id == id)
+                return &element;
     return nullptr;
 }
-template <kit::Identifiable T> T *manager2D<T>::operator[](const id_type &id)
+template <typename T> typename manager2D<T>::value_type *manager2D<T>::operator[](const id_type &id)
 {
-    for (T &element : m_elements)
-        if (element.id == id)
-            return &element;
+    if constexpr (is_scope)
+    {
+        for (T &element : m_elements)
+            if (element->id == id)
+                return element.get();
+    }
+    else
+        for (T &element : m_elements)
+            if (element.id == id)
+                return &element;
     return nullptr;
 }
 
-template <kit::Identifiable T> bool manager2D<T>::remove(const T &element)
+template <typename T> bool manager2D<T>::remove(const value_type &element)
 {
-    return remove(element.id);
+    if constexpr (kit::Indexable<T>)
+    {
+        if constexpr (is_scope)
+            return remove(element->index);
+        else
+            return remove(element.index);
+    }
+    else
+    {
+        if constexpr (is_scope)
+            return remove(element->id);
+        else
+            return remove(element.id);
+    }
 }
-template <kit::Identifiable T> bool manager2D<T>::remove(const id_type &id)
+template <typename T> bool manager2D<T>::remove(const id_type &id)
 {
-    for (std::size_t i = 0; i < m_elements.size(); ++i)
-        if (m_elements[i].id == id)
-            return remove(i);
+    if constexpr (is_scope)
+    {
+        for (std::size_t i = 0; i < m_elements.size(); ++i)
+            if (m_elements[i]->id == id)
+                return remove(i);
+    }
+    else
+        for (std::size_t i = 0; i < m_elements.size(); ++i)
+            if (m_elements[i].id == id)
+                return remove(i);
     return false;
 }
 
-template <kit::Identifiable T> std::size_t manager2D<T>::size() const
+template <typename T> std::size_t manager2D<T>::size() const
 {
     return m_elements.size();
 }
-template <kit::Identifiable T> bool manager2D<T>::empty() const
+template <typename T> bool manager2D<T>::empty() const
 {
     return m_elements.empty();
 }
+
+template class manager2D<body2D>;
+template class manager2D<collider2D>;
+template class manager2D<spring2D>;
+template class manager2D<kit::scope<behaviour2D>>;
+template class manager2D<kit::scope<constraint2D>>;
 
 } // namespace ppx
