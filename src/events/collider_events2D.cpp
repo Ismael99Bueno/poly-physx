ac5e6cc4972163ff6b5d3ae77fa457ab865d655c
Ismael Bueno <ismaelwno@gmail.com>
2024-02-24 01:21:52 +0100

    Replace find with contains when possible

--- src/events/collider_events2D.cpp
+++ src/events/collider_events2D.cpp
@@ -8,14 +8,14 @@ void collider_events2D::try_enter_or_stay(const collision2D &c, const collider2D
 {
     if (on_collision_enter.empty() && on_collision_stay.empty())
         return;
-    if (m_collided_ids.find(outcoming.id) != m_collided_ids.end())
+    if (m_collided_ids.contains(outcoming.id))
         on_collision_stay(c);
     else
         on_collision_enter(c);
 }
 void collider_events2D::try_exit(collider2D &current, collider2D &outcoming) const
 {
-    if (on_collision_stay.empty() || m_collided_ids.find(outcoming.id) == m_collided_ids.end())
+    if (on_collision_stay.empty() || !m_collided_ids.contains(outcoming.id))
         return;
     on_collision_exit(current, outcoming);
 }
