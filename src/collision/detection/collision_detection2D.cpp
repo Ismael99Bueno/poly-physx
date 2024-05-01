755e7899eba01d332be7140b38a2e7328e98e3a5
Ismael Bueno <ismaelwno@gmail.com>
2024-05-01 14:10:40 +0200

    Small rename

--- src/collision/detection/collision_detection2D.cpp
+++ src/collision/detection/collision_detection2D.cpp
@@ -137,7 +137,7 @@ void collision_detection2D::join_mt_collisions()
         m_collisions.insert(pairs.begin(), pairs.end());
 }
 
-static bool broad_collision_check(const collider2D *collider1, const collider2D *collider2)
+static bool elligible_for_collision(const collider2D *collider1, const collider2D *collider2)
 {
     const body2D *p1 = collider1->body();
     const body2D *p2 = collider2->body();
@@ -150,7 +150,7 @@ static bool broad_collision_check(const collider2D *collider1, const collider2D 
 collision2D collision_detection2D::generate_collision(collider2D *collider1, collider2D *collider2) const
 {
     collision2D collision;
-    if (!broad_collision_check(collider1, collider2))
+    if (!elligible_for_collision(collider1, collider2))
         return collision;
     if (collider1->is_circle() && collider2->is_circle())
         cc_narrow_collision_check(collider1, collider2, collision);
